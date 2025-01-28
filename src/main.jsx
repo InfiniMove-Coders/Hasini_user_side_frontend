270efe6ac63282b471a09003f3dedcbc2941fc59
TejaKumar123 <inceptioncom414@gmail.com>
2025-01-29 01:47:35 +0530

    small changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import IndexRouter from './routers/IndexRouter.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <IndexRouter />
+      <App />
     </BrowserRouter>
   </StrictMode>,
 )
